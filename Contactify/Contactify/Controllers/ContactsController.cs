9a22620e16043e81624341b58fa7701cee5703ca
Utsav Kumar <Utsavsingh611@gmail.com>
2024-05-18 11:34:27 +0530

    ContactifyProject

---
+++ Contactify/Contactify/Controllers/ContactsController.cs
@@ -0,0 +1,61 @@
+﻿using Contactify.Data;
+using Contactify.Models;
+using Contactify.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+
+namespace Contactify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactifyDbContext dbContext;
+
+        public ContactsController(ContactifyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+          var contacts = dbContext.Contacts.ToList();
+            return Ok(contacts);
+        }
+        [HttpPost]
+        public IActionResult AddContact(AddContactRequestDTO request)
+        {
+            var domainModelContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favorite = request.Favorite,
+            };
+
+            
+            dbContext.Contacts.Add(domainModelContact); 
+            dbContext.SaveChanges();
+
+            return Ok(domainModelContact);
+}
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteContact(Guid id) 
+        {
+            var contact=dbContext.Contacts.Find(id);
+            if(contact is not null)
+            {
+                dbContext.Contacts.Remove(contact);
+                dbContext.SaveChanges(true);
+            }
+            return Ok();
+
+        }
+
+        }
+    }
+ 
---
+++ Contactify/Contactify/Data/ContactifyDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Contactify.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contactify.Data
+{
+    public class ContactifyDbContext : DbContext
+    {
+        public ContactifyDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
